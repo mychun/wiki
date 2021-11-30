481d8c0c793c34d0435434031beb26d7edb00000
chun <chunxhwan@126.com>
2021-11-30 16:07:45 +0800

    1、重构启动类，把WikiApplication启动类放进config文件夹

--- src/main/java/com/chun/wiki/config/WikiApplication.java
+++ src/main/java/com/chun/wiki/config/WikiApplication.java
@@ -1,12 +1,14 @@
-package com.chun.wiki;
+package com.chun.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@ComponentScan("com.chun")
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
