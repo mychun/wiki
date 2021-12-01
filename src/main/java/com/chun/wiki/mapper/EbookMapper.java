fdff28eafa30059c725fa836dae9bde8d848583a
chun <chunxhwan@126.com>
2021-12-01 16:43:36 +0800

    1、数据库操作，使用ebookExample

--- src/main/java/com/chun/wiki/mapper/EbookMapper.java
+++ src/main/java/com/chun/wiki/mapper/EbookMapper.java
@@ -1,21 +1,31 @@
 package com.chun.wiki.mapper;
 
 import com.chun.wiki.domain.Ebook;
+import com.chun.wiki.domain.EbookExample;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
     int deleteByPrimaryKey(Long id);
 
     int insert(Ebook record);
 
     int insertSelective(Ebook record);
 
+    List<Ebook> selectByExample(EbookExample example);
+
     Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
 
     int updateByPrimaryKeySelective(Ebook record);
 
     int updateByPrimaryKey(Ebook record);
-
-    List<Ebook> list();
 }
