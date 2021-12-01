cc1519fcdf66df415e50ab2751f256b7237cdd56
chun <chunxhwan@126.com>
2021-12-01 16:47:26 +0800

    1、统一require请求类，避controller类的方法参数过多 2、统一response响应类，能控制返回前端的字段，而不去修改domain里的实体类

--- src/main/java/com/chun/wiki/controller/EbookController.java
+++ src/main/java/com/chun/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.chun.wiki.controller;
 
 import com.chun.wiki.domain.Ebook;
+import com.chun.wiki.req.EbookReq;
 import com.chun.wiki.resp.CommonResp;
 import com.chun.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
+    //http://127.0.0.1:8880/ebook/list?name=java
+    //spring boot会自动把name赋值给ebookReq.name
+    public CommonResp list(EbookReq ebookReq){
         CommonResp<Object> resp = new CommonResp<>();
-        resp.setContent(ebookService.list());
+        resp.setContent(ebookService.list(ebookReq));
         return resp;
     }
 }
--- src/main/java/com/chun/wiki/service/EbookService.java
+++ src/main/java/com/chun/wiki/service/EbookService.java
@@ -1,10 +1,16 @@
 package com.chun.wiki.service;
 
 import com.chun.wiki.domain.Ebook;
+import com.chun.wiki.domain.EbookExample;
 import com.chun.wiki.mapper.EbookMapper;
+import com.chun.wiki.req.EbookReq;
+import com.chun.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +18,22 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.list();
+    public List<EbookResp> list(EbookReq ebookReq){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        if(!ObjectUtils.isEmpty(ebookReq.getName())){
+            criteria.andNameLike("%" + ebookReq.getName() + "%");
+        }
+
+        List<Ebook> list = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> ebookResps = new ArrayList<>();
+        for (Ebook ebook : list) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            ebookResps.add(ebookResp);
+        }
+
+        return ebookResps;
     }
 }
