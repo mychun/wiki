3b855f935db5255a97c1af34939bf9a85e9f4cc5
chunxhwan <chunxhwan@126.com>
2021-12-13 14:49:15 +0800

    1、请求实体类和响应实体类，使用lombok方式定义

--- src/main/java/com/chun/wiki/req/PageReq.java
+++ src/main/java/com/chun/wiki/req/PageReq.java
@@ -7,10 +7,8 @@ import java.io.Serializable;
 
 @Data
 @Accessors(chain = true)
-public class EbookReq extends PageReq implements Serializable {
+public class PageReq implements Serializable {
     private static final long serialVersionUID = 1L;
-
-    private Long id;
-
-    private String name;
+    private int page;
+    private int size;
 }
--- src/main/java/com/chun/wiki/resp/CommonResp.java
+++ src/main/java/com/chun/wiki/resp/CommonResp.java
@@ -1,5 +1,10 @@
 package com.chun.wiki.resp;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class CommonResp<T> {
     /**
      * 业务上的成功或失败
@@ -15,38 +20,4 @@ public class CommonResp<T> {
      * 返回泛型数据，自定义类型
      */
     private T content;
-
-    public boolean getSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getContent() {
-        return content;
-    }
-
-    public void setContent(T content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("ResponseDto{");
-        sb.append("success=").append(success);
-        sb.append(", message='").append(message).append('\'');
-        sb.append(", content=").append(content);
-        sb.append('}');
-        return sb.toString();
-    }
 }
