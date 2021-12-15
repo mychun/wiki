4406fbb29f87e5c34a6f8b125f0f1bf125e6b919
chunxhwan <chunxhwan@126.com>
2021-12-15 15:37:37 +0800

    1、SQL 执行性能分析插件使用，并设置值在开发环境和测试环境使用

--- src/main/java/com/chun/wiki/config/MybatisPlusConfig.java
+++ src/main/java/com/chun/wiki/config/MybatisPlusConfig.java
@@ -3,8 +3,10 @@ package com.chun.wiki.config;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MybatisPlusConfig {
@@ -22,5 +24,18 @@ public class MybatisPlusConfig {
     @Bean
     public ISqlInjector sqlInjector(){
         return new LogicSqlInjector();
+    }
+
+    /**
+     * SQL 执行性能分析插件
+     * 开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长
+     */
+    @Bean
+    @Profile({"dev", "test"})
+    public PerformanceInterceptor performanceInterceptor(){
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(100);//ms，超过此处设置的ms则sql不执行
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
     }
 }
